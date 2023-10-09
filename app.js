9d8fa0a42ca85075db8385b16887abce982a24f1
Kacper Bujak <kaccpper2@gmail.com>
2023-10-09 10:31:31 +0200

    .

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ require('./auth/passport');
 
 const app = express();
 
+app.use(cors());
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -26,10 +28,8 @@ async function main() {
   await mongoose.connect(mongoDB);
 }
 
-app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
-
 
 // app.use(function(req, res, next) {
 //   const err = new Error('Not found');
